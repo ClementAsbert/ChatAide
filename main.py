21cc6a04236369ccb6c6d21f1b8320957be6f7c9
ClementAsbert <57488829+ClementAsbert@users.noreply.github.com>
2020-11-18 13:14:03 +0100

    Création d'un label Utilisateur et modification de la fonction envoyer

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from mainwindow import Ui_MainWindow
+from Utilisateur import Utilisateur
 from PyQt5 import QtWidgets
 
 class Chat(QtWidgets.QMainWindow):
@@ -9,17 +10,24 @@ class Chat(QtWidgets.QMainWindow):
         super(Chat, self).__init__(parent)
         self.main = QtWidgets.QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
+        self.utilisateur = Utilisateur()
         self.ui.setupUi(self)
         self._initSlotButton()
         
         
     def _initSlotButton (self):
-        self.ui.envoyer.clicked.connect(self.textEnvoyer)
+        self.ui.envoyer.clicked.connect(self.Envoyer)
 
-    def textEnvoyer(self):
+
+    def Envoyer(self):
         msg = self.ui.textUtilisateur.toPlainText()
+        self.ui.labelUtilisateur.setText(self.utilisateur.name)
+        self.ui.textEdit.append(self.ui.labelUtilisateur.text())
+        self.ui.textEdit.insertPlainText(" > ")
         self.ui.textEdit.append(msg)
+        self.ui.textEdit.append("")
         self.ui.textUtilisateur.clear()
+
 
 
 if __name__ == "__main__":
