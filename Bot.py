8a4d6c6dd8318ccb4a308b56b811bf1e67f18ffa
ClementAsbert <57488829+ClementAsbert@users.noreply.github.com>
2020-11-18 13:12:37 +0100

    Ajout de la classe Bot et utilisateur

---
+++ Bot.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class Bot():
+    def __init__(self):
+        self.__name = "Bot"
+
+
+    @property
+    def name(self):
+        return self.__name 
---
+++ Utilisateur.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+class Utilisateur():
+    def __init__(self):
+
+        self.__name = "Utilisateur"
+
+
+
+    @property
+    def name(self):
+        return self.__name
+    
+    
